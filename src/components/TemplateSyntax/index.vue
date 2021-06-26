445727e25c599087be81d47c40d78f22925f05fa
Mohamed Tajjiou <mtajjiou@sqli.com>
2021-06-26 14:57:26 +0200

    feat(templatesyntax): example solution

--- src/components/TemplateSyntax/index.vue
+++ src/components/TemplateSyntax/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="tempalte-syntax"></div>
+  <div class="tempalte-syntax">
+    <div class="msg">should display message: {{ msg }}</div>
+    <div class="html" v-html="html" />
+    <div class="id" :id="id">this element has an id</div>
+    <div class="visible" v-show="visible" @click="toggleElement">
+      this element should be toggled on click
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,11 +14,15 @@
 // 2. you should use html directive to render raw html using the html state
 // 3. you should use binding directive to set id using the id from the state
 // 4. you should conditionaly render (show in the first render) the visible element using the visible state
-
 export default {
   name: "template-syntax",
   data() {
-    return {};
+    return {
+      msg: "interpolated message",
+      html: "<b>Bold Text</b>",
+      id: "element-id",
+      visible: true,
+    };
   },
   methods: {
     toggleElement() {
