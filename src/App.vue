af1d26f5385e1ba89f925bc3215064cb8a6d4ef4
Mohamed Tajjiou <mtajjiou@sqli.com>
2021-06-26 13:42:49 +0200

    feat(app): check virtual dom

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <h1>this should be not updated</h1>
+    <button v-on:click="incrementCount">Increament</button>
+    <p>count {{ count }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    incrementCount() {
+      this.count++;
+    },
+  },
 };
 </script>
 
