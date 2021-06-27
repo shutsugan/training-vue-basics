a830f98127246589a058b05b41a0746da3dbb931
Mohamed Tajjiou <mtajjiou@sqli.com>
2021-06-27 16:00:31 +0200

    test(app): create unit test for component props

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,22 @@
 </template>
 
 <script>
+// create a components folder which will contain the components
+
+//1. create a component that expect a message prop and displays it
+//2. create a component that expect a number which should be used as an initial state for the counter
+// and by clicking the button the count should increament without mutating the prop
+//3. create a component that expect a list a prop and displays the items
+
 export default {
   name: "App",
+  data() {
+    return {
+      message: "The message to be dispalyed",
+      initValue: 5,
+      list: ["item 1", "item 2", "item 2"],
+    };
+  },
 };
 </script>
 
