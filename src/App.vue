638ee9b76324e30a3b2c72d9d7551de852d105a1
Mohamed Tajjiou <mtajjiou@sqli.com>
2021-06-27 16:46:26 +0200

    test(calc): create unit test for calc component

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,18 @@
 </template>
 
 <script>
+//1. create a component that a number prop, when clicking on the calculate button
+// the result should be returned to App to set powerOfNumber state that should be used as a prop
+// to be display in the component
+
 export default {
   name: "App",
+  data() {
+    return {
+      number: 3,
+      powerOfNumber: 0,
+    };
+  },
 };
 </script>
 
