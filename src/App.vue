ed9e05d0d4aaf389450a9c7cac81108d448ec736
Mohamed Tajjiou <mtajjiou@sqli.com>
2021-06-27 21:34:04 +0200

    feat(hooks): add solution

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <hooks />
+  </div>
 </template>
 
 <script>
 //1. create a component that will display the current Date on mount
 //2. add a counter that will trigger one update and log the update count to the state
+import Hooks from "./Hooks.vue";
 export default {
   name: "App",
+  components: { Hooks },
 };
 </script>
 
---
+++ src/Hooks.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button ref="update" @click="handleUpdate">Update</button>
+    <div ref="mount">{{ mountTime }}</div>
+    <div>{{ count }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hooks",
+  data() {
+    return {
+      mountTime: "",
+      count: 0,
+      updateTimes: 0,
+    };
+  },
+  mounted() {
+    this.mountTime = new Date().toDateString();
+  },
+  updated() {
+    this.updateTimes++;
+  },
+  methods: {
+    handleUpdate() {
+      this.count++;
+    },
+  },
+};
+</script>
+
+<style></style>
